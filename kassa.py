b1622823dbb9816eb9f526118d6918e1c775b0a6
samkdt <sam.kdt99@gmail.com>
2019-12-09 13:32:19 +0100

    la till + och - knapp samt ångra knapp

--- kassa.py
+++ kassa.py
@@ -1,26 +1,49 @@
 import tkinter as tk
 
+class art:
+	def __init__(self, namn, moms):
+		self.namn=namn
+		self.moms=moms
+
+class prod:
+	def __init__(self, namn, antal, pris, moms):
+		self.namn=namn	
+		self.antal=antal
+		self.pris=pris	
+		self.moms=moms
+		self.totpris=pris*antal
+
+
 hj=1080
 br=1920
 num=0
 kvitto=[]
-
-class prod:
-	def __init__(self, namn, antal, pris):
-		self.namn=namn	
-		self.antal=antal
-		self.pris=pris	
-		self.totpris=pris*antal
+artiklar=[art("livsmedel",12), art("godis",12), art("hygien",25), art("dricka",12)]
 
 def update_input():
 	text['text']= num
 
 def update_display():
+
+	antal['text']="antal\n"
+	namn['text']="namn\n"
+	pris['text']="pris\n"
+	totpris['text']="pris f.a.\n"
+
 	for x in range(0,len(kvitto)):
 		antal['text']+=str(kvitto[x].antal) + "\n"
 		namn['text']+=kvitto[x].namn + "\n"
 		pris['text']+=str(kvitto[x].pris) + "\n"
 		totpris['text']+=str(kvitto[x].totpris) + "\n"
+
+def knapp_art(artnr):
+	global num
+	global artiklar
+	if(num!=0):
+		kvitto.append(prod(artiklar[artnr].namn,1,num,artiklar[artnr].moms))
+		num=0
+	update_input()
+	update_display()
 
 def knapp_del():
 	global num
@@ -41,13 +64,37 @@ def knap_tryck(inp):
 	num+=inp
 	update_input()
 	
-	
+def knapp_minus():
+	global kvitto
+	global num
+	if(num == 0):
+		kvitto[len(kvitto)-1].antal-=1
+	else:
+		kvitto[len(kvitto)-1].antal-=num
+	num=0
+	kvitto[len(kvitto)-1].totpris=kvitto[len(kvitto)-1].pris*kvitto[len(kvitto)-1].antal
+	update_input()
+	update_display()
+
+def knapp_plus():
+	global kvitto
+	global num
+	if(num == 0):
+		kvitto[len(kvitto)-1].antal+=1
+	else:
+		kvitto[len(kvitto)-1].antal+=num
+	num=0
+	kvitto[len(kvitto)-1].totpris=kvitto[len(kvitto)-1].pris*kvitto[len(kvitto)-1].antal
+	update_input()
+	update_display()
+
+def knapp_remove():
+	global kvitto
+	kvitto.pop(len(kvitto)-1)
+	update_display()	
+
 
 root = tk.Tk()
-
-kvitto.append(prod( "bajs", 2, 50))
-kvitto.append(prod( "mjölk", 3, 19))
-kvitto.append(prod( "horor", 1, 200))
 
 
 #avkomentera för full skärm
@@ -58,11 +105,49 @@ kvitto.append(prod( "horor", 1, 200))
 wind = tk.Canvas(root, height=hj, width=br)
 wind.pack()
 
+#instälningar typ?
+set=tk.Frame(root, bg="#ff00ff", bd=5)
+set.place(relx=0, rely=0.75, relheight=0.25, relwidth=0.5)
+
+knapp_mer=tk.Button(set, text="+", command=lambda: knapp_plus())
+knapp_mer.place(relx=0,rely=0,relheight=0.5,relwidth=0.333)
+
+knapp_min=tk.Button(set, text="-", command=lambda: knapp_minus())
+knapp_min.place(relx=0.333,rely=0,relheight=0.5,relwidth=0.333)
+
+knapp_undo=tk.Button(set, text="ångra", command=lambda: knapp_remove())
+knapp_undo.place(relx=0.666,rely=0,relheight=0.5,relwidth=0.333)
+
+
+knapp_kont=tk.Button(set, text="kontant")
+knapp_kont.place(relx=0,rely=0.5,relheight=0.5,relwidth=0.5)
+
+knapp_kort=tk.Button(set, text="kort")
+knapp_kort.place(relx=0.5,rely=0.5,relheight=0.5,relwidth=0.5)
+
+
+#artiklar
+artik=tk.Frame(root, bg="#ff00ff", bd=5)
+artik.place(relx=0.5,rely=0.75,relheight=0.25,relwidth=0.5)
+
+art_0=tk.Button(artik, text=artiklar[0].namn, command=lambda: knapp_art(0))
+art_0.place(relx=0, rely=0,relheight=0.5,relwidth=0.5)
+
+art_1=tk.Button(artik, text=artiklar[1].namn, command=lambda: knapp_art(1))
+art_1.place(relx=0.5, rely=0,relheight=0.5,relwidth=0.5)
+
+art_2=tk.Button(artik, text=artiklar[2].namn, command=lambda: knapp_art(2))
+art_2.place(relx=0.0, rely=0.5,relheight=0.5,relwidth=0.5)
+
+art_3=tk.Button(artik, text=artiklar[3].namn, command=lambda: knapp_art(3))
+art_3.place(relx=0.5, rely=0.5,relheight=0.5,relwidth=0.5)
+
+
 #kvitto display
 kvitt = tk.Frame(root, bg="#ff00ff", bd=5)
 kvitt.place(relx=0, rely=0, relheight=0.75, relwidth=0.5)
 
-antal=tk.Label(kvitt,text="antal\n", anchor="nw")
+antal=tk.Label(kvitt,text="antal\n", anchor="nw", bg="#dddddd")
 antal.place(relx=0, rely=0, relheight=1, relwidth=0.1)
 
 namn=tk.Label(kvitt,text="namn\n", anchor="nw")
